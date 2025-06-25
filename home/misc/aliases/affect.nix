ca971dc6acb184d2875faeeddd9ab677adcd3127
Mauricio Barg <mrwbarg@gmail.com>
2025-06-25 10:54:35 -0300

    Fix aliases

--- home/misc/aliases/affect.nix
+++ home/misc/aliases/affect.nix
@@ -7,11 +7,11 @@
   home.shellAliases = {
     ssh-staging = "aptible ssh --app=homebase-staging";
     ssh-prod = "aptible ssh --app=homebase-two-prod";
-    db-prod = "aft rds tunnel -d production";
-    db-staging = "aft rds tunnel -d staging";
-    db-replica = "aft rds tunnel -d replica";
-    wh-prod = "aft rds tunnel -d production";
-    rh-prod = "aft rds tunnel -d redhouse-prod";
+    db-prod = "TMPDIR=/tmp aft rds tunnel -d production";
+    db-staging = "TMPDIR=/tmp aft rds tunnel -d staging";
+    db-replica = "TMPDIR=/tmp aft rds tunnel -d replica";
+    wh-prod = "TMPDIR=/tmp aft rds tunnel -d production";
+    rh-prod = "TMPDIR=/tmp aft rds tunnel -d redhouse-prod";
     runserver = "service django runserver";
     venv = "source .venv/bin/activate.fish";
   };
